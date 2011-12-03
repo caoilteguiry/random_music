93bdc39c83a4c17e9f73b1ac879078fac8337159
Caoilte Guiry <caoilte.guiry@gmail.com>
2011-12-03 19:43:48 +0000

    Moving to a package and creating a setup.py. Updating the README

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from random_music.random_music import __version__
+
+setup(
+    name = u"random_music",
+    version = __version__,
+    author = u"Caoilte Guiry",   
+    author_email = u"", 
+    description = u"Plays a pseudo-random sequence of songs",
+    long_description = open(u'README').read(),
+    packages = find_packages(),
+    entry_points = {
+        'console_scripts': ['music = random_music.random_music:main']
+    },
+)
