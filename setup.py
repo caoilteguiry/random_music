1795de8cf64be8b99338037d64c24826f6754f58
Caoilte Guiry <caoilte.guiry@gmail.com>
2012-12-30 19:43:26 +0000

    Removing whitespace between fn args, using context manager for file reading

--- setup.py
+++ setup.py
@@ -4,16 +4,20 @@
 from setuptools import setup, find_packages
 from random_music.random_music import __version__
 
+
+with open(u'README') as fh:
+    long_description = fh.read()
+
 setup(
-    name = u"random_music",
-    version = __version__,
-    author = u"Caoilte Guiry",   
-    author_email = u"", 
-    license='BSD License',
-    description = u"Plays a pseudo-random sequence of songs",
-    long_description = open(u'README').read(),
-    packages = find_packages(),
-    entry_points = {
+    name=u"random_music",
+    version=__version__,
+    author=u"Caoilte Guiry",   
+    author_email=u"", 
+    license=u'BSD License',
+    description=u"Plays a pseudo-random sequence of songs",
+    long_description=long_description,
+    packages=find_packages(),
+    entry_points={
         'console_scripts': ['music = random_music.random_music:main']
     },
 )
